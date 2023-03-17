a5511cca04dc43f1ade477dae002ac76101e9f19
Neha Singh <neha.singh931126@gmail.com>
2023-03-17 15:34:29 +0530

    Added the nodemon and .env file

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
 
 app.use(express.json());
 
+dotenv.config();
+
 const routes = require('./api_routes/routes');
 routes(app);
-const port = process.env.APP_PORT || 3000;
+const port = process.env.APP_PORT || 0000;
 
 app.listen(port, () => {
     console.log(`Listening to port http://localhost:${port}`);
