b15a197de5a43f25479f04b91238d833f80a5dfe
Neha Singh <neha.singh931126@gmail.com>
2023-03-17 15:55:25 +0530

    config changes

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv');
+const { APP_PORT} = require('./config/config');
+
 
 app.use(express.json());
 
-dotenv.config();
-
 const routes = require('./api_routes/routes');
 routes(app);
-const port = process.env.APP_PORT || 0000;
 
-app.listen(port, () => {
-    console.log(`Listening to port http://localhost:${port}`);
+app.listen(APP_PORT, () => {
+    console.log(`Listening to port http://localhost:${APP_PORT}`);
 });
